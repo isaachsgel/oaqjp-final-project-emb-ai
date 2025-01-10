9cc6c91035248a8755a7b029a6e7a2f81d3fdbaa
Isaach <117325207+isaachsgel@users.noreply.github.com>
2025-01-09 23:04:38 -0500

    Create setup.py

---
+++ EmotionDetection/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EmotionDetection',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=['requests'],  # Add dependencies here
+    description='A package for emotion detection using Watson API',
+    author='I S',
+    author_email='isaac.sagel@utp.ac.pa',
+    url='https://github.com/isaachsgel/EmotionDetection',  # Replace with actual URL if needed
+)
